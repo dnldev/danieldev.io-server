a4f21463c6fb91d1867d26f63cf8d1cc5538729c
unknown <daniel.ungersboeck@gmail.com>
2018-07-20 12:10:36 +0200

    add read only graphql route add authentication for mutations

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+const server = express();
+
 const graphqlHTTP = require('express-graphql');
 
 const mongoose = require('mongoose');
-
 mongoose.connect(
   'mongodb://localhost:27017/freelance-page',
   { useNewUrlParser: true }
@@ -11,16 +11,42 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
-const server = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+server.use(bodyParser.json());
+server.use(cors());
+
 const PORT = 4000;
 
-const schema = require('./schema');
+const { queries, mutations } = require('./schema');
 
-server.use(cors());
+const { authenticate, authorization } = require('./authentication');
+
+server.post('/auth', (req, res) => {
+  authenticate(req.body.password)
+    .then(id => {
+      res.json({ token: id });
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(401);
+    });
+});
+
 server.use(
-  '/graphql',
+  '/graphql/queries',
   graphqlHTTP({
-    schema,
+    schema: queries,
+    graphiql: true,
+  })
+);
+
+server.use(
+  '/graphql/mutations',
+  authorization,
+  graphqlHTTP({
+    schema: mutations,
     graphiql: true,
   })
 );
